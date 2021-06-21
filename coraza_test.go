828bd640ba598685f361738fb476c369420e6485
Juan Pablo Tosso <jptosso@gmail.com>
2021-06-21 01:37:25 -0400

    must fix coraza interrupts

--- coraza_test.go
+++ coraza_test.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 	"testing"
 	"time"
+	"net/http"
 )
 
 func TestPlugin(t *testing.T) {
@@ -37,6 +38,22 @@ func TestPluginReload(t *testing.T) {
 
 	tester.InitServer(rawConfig, "caddyfile")
 	tester.AssertGetResponse(baseURL+"/test", 200, "test456")
+
+	time.Sleep(1 * time.Second)
+}
+
+func TestSimpleRule(t *testing.T) {
+	tester := caddytest.NewTester(t)
+	baseURL := "http://127.0.0.1:8080"
+	configFile := "test/caddyfile"
+	configContent, err := ioutil.ReadFile(configFile)
+	if err != nil {
+		t.Fatalf("Failed to load configuration file %s: %s", configFile, err)
+	}
+	rawConfig := string(configContent)
+	tester.InitServer(rawConfig, "caddyfile")
+	req, _ := http.NewRequest("GET", baseURL+"/test5", nil)
+	tester.AssertResponseCode(req, 500)
 
 	time.Sleep(1 * time.Second)
 }
