bfbee645de6f88e6b75fc26e83f134fc81d774b1
Juan Pablo Tosso <jptosso@gmail.com>
2022-07-26 12:30:40 -0400

    unexport non-required functions

--- caddy/main.go
+++ caddy/main.go
@@ -19,9 +19,6 @@ import (
 
 	_ "github.com/caddyserver/caddy/v2/modules/standard"
 	_ "github.com/corazawaf/coraza-caddy"
-
-	// You may uncomment the following lines to enable pcre plugins (if you need use crs rules)
-	// _ "github.com/jptosso/coraza-pcre"
 )
 
 func main() {
